6332e786fdd29440a78510e49afd7e5ce64fe284
HiD <salomone.marco@outlook.it>
2020-11-03 21:59:23 +0100

    Implemented the Graph exercise, the last one

--- src/it/unibo/oop/lab06/generics1/GraphImpl.java
+++ src/it/unibo/oop/lab06/generics1/GraphImpl.java
@@ -1,38 +1,87 @@
 package it.unibo.oop.lab06.generics1;
 
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
 import java.util.Set;
 
 public class GraphImpl<N> implements Graph<N> {
 
+	private Set<N> nodes;
+	private Map<N,Set<N>> edges;
+	
+	public GraphImpl() {
+		this.nodes = new HashSet<N>();
+		this.edges = new HashMap<N,Set<N>>();
+	}
+	
+	
 	@Override
 	public void addNode(N node) {
-		// TODO Auto-generated method stub
-		
+		nodes.add(node);
 	}
 
 	@Override
 	public void addEdge(N source, N target) {
-		// TODO Auto-generated method stub
 		
+		if (source == null || target == null) {
+			return;
+		}
+		
+		if (!edges.containsKey(source)) {
+			Set<N> edgesList = new HashSet<N>();
+			edgesList.add(target);
+			this.edges.put(source, edgesList);
+		}
+		else {
+			Set<N> edgesList = edges.get(source);
+			edgesList.add(target);
+		}
 	}
 
 	@Override
 	public Set<N> nodeSet() {
-		// TODO Auto-generated method stub
-		return null;
+		return new HashSet<N>(nodes);
 	}
 
 	@Override
 	public Set<N> linkedNodes(N node) {
-		// TODO Auto-generated method stub
-		return null;
+		if (edges.containsKey(node)) {
+			return new HashSet<N>(edges.get(node));
+		}
+		return new HashSet<N>();
 	}
 
 	@Override
 	public List<N> getPath(N source, N target) {
-		// TODO Auto-generated method stub
-		return null;
+		LinkedList<N> returnList = new LinkedList<N>();
+		HashSet<N> triedNodes = new HashSet<N>();
+		getPath(source,target,returnList, triedNodes);
+		returnList.addFirst(source);
+		return returnList;
+	}
+	
+	private boolean getPath(N source, N target, LinkedList<N> appendList, HashSet<N> triedNodes) {
+		if (source == target) {
+			return true;
+		}
+		if (triedNodes.contains(source)) {
+			triedNodes = new HashSet<N>();
+			return false;
+		}
+		triedNodes.add(source);
+		
+		for (N adjacent : this.edges.get(source)) {
+			if (getPath(adjacent,target,appendList, triedNodes)) {
+				appendList.addFirst(adjacent);
+				return true;
+			}
+		}
+		
+		return false;
 	}
 
 }
